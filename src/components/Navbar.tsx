2bd40122db7d83f5039f84634b4c350db6f3fdfa
le_nichols@yahoo.com <le_nichols@yahoo.com>
2025-04-05 20:49:45 -0700

    cleanup menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,110 +13,121 @@ export function NavbarComponent() {
 
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600 shadow-md">
-      <div className="max-w-screen-xl flex md:flex-wrap lg:flex-wrap items-center justify-between mx-auto p-4">
-        <div className="flex items-center justify-center rtl:space-x-reverse">
-          <Image
-            src="/images/MillenniumAutoRetailers.png"
-            alt="Millennium Auto Logo"
-            layout="responsive"
-            width={329}
-            height={205}
-            sizes="(max-width: 768px) 40vw, 14vw"
-            className="sm:mx-auto"
-          />
-          <div className="self-center text-sm md:text-sm lg:text-lg sm:whitespace-normal lg:whitespace-nowrap dark:text-orange-500 pl-4 block">17065 Tennessee Street - Vallejo, CA 94590&nbsp;&nbsp; <span><PhoneCall size={20} className="inline-block" /> <a href={`tel:707-332-9005`} className="font-bold border-none">(707) 332-9005</a></span>
+    <div className="max-w-screen-xl mx-auto px-4 py-4">
+      <div className="relative flex flex-col md:flex-row md:items-center md:justify-between">
+        {/* Logo and Address */}
+        <div className="flex flex-col items-center md:flex-row md:items-center md:space-x-4 w-full">
+          <div className="flex flex-col items-center md:flex-row md:items-center md:space-x-4">
+            <Image
+              src="/images/MillenniumAutoRetailers.png"
+              alt="Millennium Auto Logo"
+              width={140}
+              height={87}
+              className="h-auto w-[140px]"
+            />
+            <div className="text-center md:text-left text-sm md:text-sm lg:text-lg dark:text-orange-500 mt-2 md:mt-0">
+              17065 Tennessee Street - Vallejo, CA 94590&nbsp;&nbsp;
+              <span className="whitespace-nowrap">
+                <PhoneCall size={20} className="inline-block" />{" "}
+                <a href="tel:707-332-9005" className="font-bold border-none">
+                  (707) 332-9005
+                </a>
+              </span>
+            </div>
           </div>
         </div>
-        <button
-          data-collapse-toggle="navbar-dropdown"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-dropdown"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        {/* <div className="self-center hidden sm:flex text-sm dark:text-orange-500 pl-4 block">17065 Tennessee Street - Vallejo, CA 94590&nbsp;&nbsp; <PhoneCall size={20} className="inline-block" /> <a href={`tel:707-332-9005`} className="font-bold border-none">(707) 332-9005</a>
-        </div> */}
-        <div className="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 mr-8 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
+  
+        {/* Hamburger Menu (Mobile only) */}
+        <div className="absolute right-0 top-0 md:hidden">
+          <button
+            data-collapse-toggle="navbar-dropdown"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-dropdown"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+  
+        {/* Desktop Menu */}
+        <div className="hidden md:block mt-4 md:mt-0" id="navbar-dropdown">
+        <ul className="flex flex-col md:flex-row md:flex-nowrap md:space-x-8 font-medium p-4 md:p-0 border border-gray-100 md:border-0 rounded-lg bg-gray-50 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700 whitespace-nowrap">
+        <li>
               <Link
                 href="#"
                 className="block py-2 px-3 text-white bg-orange-500 rounded-sm md:bg-transparent md:text-orange-500 md:p-0 md:dark:text-orange-400 dark:bg-orange-400 md:dark:bg-transparent"
-                aria-current="page"
               >
                 Inventory & Financing
               </Link>
             </li>
-            {/* <li>
-              <button
-                id="dropdownNavbarLink"
-                data-dropdown-toggle="dropdownNavbar"
-                className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 md:w-auto dark:text-white md:dark:hover:text-orange-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
-              >
-                Dropdown{" "}
-                <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
-                </svg>
-              </button>
-              <div
-                id="dropdownNavbar"
-                suppressHydrationWarning
-                className="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600"
-              >
-                <ul className="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
-                  <li>
-                    <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                      Dashboard
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                      Settings
-                    </Link>
-                  </li>
-                  <li>
-                    <Link href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                      Earnings
-                    </Link>
-                  </li>
-                </ul>
-                <div className="py-1">
-                  <Link href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
-                    Sign out
-                  </Link>
-                </div>
-              </div>
-            </li> */}
             <li>
-              <Link href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+              <Link
+                href="#"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
                 Other Fleet
               </Link>
             </li>
-            {/* <li>
-              <Link href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                Financing
-              </Link>
-            </li> */}
             <li>
-              <Link href="#contact_us" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+              <Link
+                href="#contact_us"
+                className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-orange-500 md:p-0 dark:text-white md:dark:hover:text-orange-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
                 Contact Us
               </Link>
             </li>
           </ul>
         </div>
       </div>
-    </nav>
+  
+      {/* Mobile Dropdown */}
+      <div className="hidden mt-4 md:hidden" id="navbar-dropdown">
+        <ul className="flex flex-col font-medium p-4 border border-gray-100 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700 space-y-2">
+          <li>
+            <Link
+              href="#"
+              className="block py-2 px-3 text-white bg-orange-500 rounded-sm"
+            >
+              Inventory & Financing
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="#"
+              className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Other Fleet
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="#contact_us"
+              className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
   );
 }
 
